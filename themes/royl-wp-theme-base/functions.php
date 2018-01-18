b38f9fceb879e0a063e50898df4959a635776b52
Roy Lindauer <roy.lindauer@noblestudios.com>
2018-01-18 09:00:43 -0800

    clean up namepsace use in functions.php

--- themes/royl-wp-theme-base/functions.php
+++ themes/royl-wp-theme-base/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+Use \Royl\WpThemeBase\Util;
+
 // Load the Autoloader
 require_once 'inc/autoload.php';
 
@@ -11,13 +13,13 @@ require_once 'inc/template-tags.php';
 
 // Setup default theme config
 include_once 'inc/config-default.php';
-\Royl\WpThemeBase\Util\Configure::set($config);
+Util\Configure::set($config);
 
 // Set some theme specific configurations in the global Config
 if (function_exists('wp_get_theme')) {
     $curtheme = wp_get_theme();
-    \Royl\WpThemeBase\Util\Configure::write('name', $curtheme->get('Name'));
-    \Royl\WpThemeBase\Util\Configure::write('domain', $curtheme->get('TextDomain'));
-    \Royl\WpThemeBase\Util\Configure::write('version', $curtheme->get('Version'));
+    Util\Configure::write('name', $curtheme->get('Name'));
+    Util\Configure::write('domain', $curtheme->get('TextDomain'));
+    Util\Configure::write('version', $curtheme->get('Version'));
     unset($curtheme);
 }
